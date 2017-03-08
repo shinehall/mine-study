4042d09475cde32d8fc62af3ecd54af3f9226900
chauncy <hs1104@163.com>
2017-03-08 10:06:56 +0800

    nothing

--- src/main/java/tech/thread/training/MainThreadTest.java
+++ src/main/java/tech/thread/training/MainThreadTest.java
@@ -3,7 +3,6 @@ package tech.thread.training;
 public class MainThreadTest {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     Thread t = Thread.currentThread();
     System.out.println("current thread name: " + t);
     t.setName("new thread");
